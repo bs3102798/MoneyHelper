f9d0563d23a469559e6497051954c421e7cb0755
Burness Scott III <bsiii102798@gmail.com>
2024-10-29 20:52:36 -0700

    api server & name,username,password useStates

--- api/index.js
+++ api/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const cors = require('cors')
 const app = express();
 
+app.use(cors({
+    credentials: true,
+    origin: 'http://localhost:5173',
+}));
+
 app.get('/test', (req, res) => {
-    res.json('test ok')
+    res.json('test ok i work fine')
 });
 
 app.listen(4000, () => {
